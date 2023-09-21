a9d17df60f11ea6887a1a342f31c933722e36d00
Haian Barbosa <141703525+haiDbarbosa@users.noreply.github.com>
2023-09-21 18:06:00 -0400

    added sidemenu buttons to loop

--- fitlife/scripts/headerinteraction.js
+++ fitlife/scripts/headerinteraction.js
@@ -2,6 +2,12 @@
 const menuButton = document.getElementsByClassName("menu")[0]
 const aside = document.getElementsByTagName("aside")[0]
 const background = document.getElementsByClassName("background")[0]
+const botao = document.getElementsByTagName("a")[6]
+const botao1 = document.getElementsByTagName("a")[7]
+const botao2 = document.getElementsByTagName("a")[8]
+const botao3 = document.getElementsByTagName("a")[9]
+const botao4 = document.getElementsByTagName("a")[10]
+const botao5 = document.getElementsByTagName("a")[11]
 
 let showmenu = false;
 
@@ -10,6 +16,30 @@ menuButton.addEventListener("click" , () => {
 })
 
 background.addEventListener("click" , () => {
+    handleChange();
+})
+
+botao.addEventListener("click" , () => {
+    handleChange();
+})
+
+botao1.addEventListener("click" , () => {
+    handleChange();
+})
+
+botao2.addEventListener("click" , () => {
+    handleChange();
+})
+
+botao3.addEventListener("click" , () => {
+    handleChange();
+})
+
+botao4.addEventListener("click" , () => {
+    handleChange();
+})
+
+botao5.addEventListener("click" , () => {
     handleChange();
 })
 
