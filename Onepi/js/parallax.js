a1b63c9714e3a1b4d65a7907f11809d855374916
Haian Barbosa <141703525+haiDbarbosa@users.noreply.github.com>
2023-10-07 23:27:39 -0400

    edited logo X movement

--- Onepi/js/parallax.js
+++ Onepi/js/parallax.js
@@ -17,9 +17,8 @@ function parallax(e){
     .forEach(layer =>{
         const speed = layer.getAttribute('data-speed')
     
-        const x = (window.innerWidth - e.pageX*speed)/120
         const y = (window.innerHeight - e.pageY*speed)/100
 
-    layer.style.transform = `translateX(${x}px) translateY(${y}px)`
+    layer.style.transform = `translateY(${y}px)`
 })
 }
