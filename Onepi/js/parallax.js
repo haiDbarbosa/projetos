9fc03e4b73f2f9b26d0a8c829f28caab615b16a4
Haian Barbosa <141703525+haiDbarbosa@users.noreply.github.com>
2023-10-07 23:29:52 -0400

    logo parallax movement ratio update

--- Onepi/js/parallax.js
+++ Onepi/js/parallax.js
@@ -17,7 +17,7 @@ function parallax(e){
     .forEach(layer =>{
         const speed = layer.getAttribute('data-speed')
     
-        const y = (window.innerHeight - e.pageY*speed)/100
+        const y = (window.innerHeight - e.pageY*speed)/70
 
     layer.style.transform = `translateY(${y}px)`
 })
